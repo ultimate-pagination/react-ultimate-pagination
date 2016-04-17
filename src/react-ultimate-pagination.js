91d8523708f632c573a017e763c4bd2e66432f6b
Dmytro Yarmak <dmytroyarmak@gmail.com>
2016-04-18 00:17:01 +0300

    Basic paginator implementation (work in progress)

---
+++ src/react-ultimate-pagination.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {getPaginationModel} from 'ultimate-pagination';
+
+const UltimatePagination = ({currentPage, totalPages}) => {
+  const paginationModel = getPaginationModel(currentPage, totalPages);
+  return (
+    <div>
+      {
+        paginationModel.map((item, index) => (
+          <button key={index}>{item}</button>
+        ))
+      }
+    </div>
+  );
+};
+
+UltimatePagination.propTypes = {
+  currentPage: React.PropTypes.number.isRequired,
+  totalPages: React.PropTypes.number.isRequired
+};
+
+export UltimatePagination;
