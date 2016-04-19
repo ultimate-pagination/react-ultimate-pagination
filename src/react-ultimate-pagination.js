880dd616eefa2592df44f9a2b4965c8a608d812f
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-19 23:31:58 +0300

    Add onChange attribute

--- src/react-ultimate-pagination.js
+++ src/react-ultimate-pagination.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {getPaginationModel} from 'ultimate-pagination';
 
-const UltimatePagination = ({currentPage, totalPages}) => {
+const isEllipsis = item => item === '...';
+
+const UltimatePagination = ({currentPage, totalPages, onChange}) => {
   const paginationModel = getPaginationModel(currentPage, totalPages);
+  const onPageClick = (item) => {
+    return () => {
+      if (onChange && currentPage !== item) {
+        onChange(item);
+      }
+    };
+  };
+
   return (
     <div>
       {
         paginationModel.map((item, index) => (
-          <button key={index}>{item}</button>
+          <button key={index} disabled={isEllipsis(item)} onClick={onPageClick(item)}>{item}</button>
         ))
       }
     </div>
@@ -16,7 +26,8 @@ const UltimatePagination = ({currentPage, totalPages}) => {
 
 UltimatePagination.propTypes = {
   currentPage: React.PropTypes.number.isRequired,
-  totalPages: React.PropTypes.number.isRequired
+  totalPages: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func
 };
 
 export {UltimatePagination};
