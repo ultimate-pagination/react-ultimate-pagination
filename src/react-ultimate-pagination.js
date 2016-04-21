63a8b648ca392645d37f2ee2cdf0d07d369df521
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-21 22:35:23 +0300

    Make createUltimatePagination high order component

--- src/react-ultimate-pagination.js
+++ src/react-ultimate-pagination.js
@@ -1,53 +1,36 @@
 import React from 'react';
 import {getPaginationModel, ITEM_TYPES} from 'ultimate-pagination';
 
-const activePageButtonStyle = {fontWeight: 'bold'};
-
-const LABEL_BY_ITEM_TYPE = {
-  [ITEM_TYPES.ELLIPSIS]: '...',
-  [ITEM_TYPES.FIRST_PAGE_LINK] : '<<',
-  [ITEM_TYPES.PREVIOS_PAGE_LINK] : '<',
-  [ITEM_TYPES.NEXT_PAGE_LINK] : '>',
-  [ITEM_TYPES.LAST_PAGE_LINK] : '>>'
-};
-
-const isActivePage = (item) => item.isActive && item.type === ITEM_TYPES.PAGE;
-const isDisabled = (item) => item.type === ITEM_TYPES.ELLIPSIS || (item.type !== ITEM_TYPES.PAGE && item.isActive);
-const getButtonStyle = (item) => isActivePage(item) ? activePageButtonStyle : null;
-const getItemLabel = (item) => item.type === ITEM_TYPES.PAGE ? item.value : LABEL_BY_ITEM_TYPE[item.type];
-
-const UltimatePagination = ({currentPage, totalPages, onChange}) => {
-  const paginationModel = getPaginationModel({currentPage, totalPages});
-  const onPageClick = ({value}) => {
+const renderItemComponentFunctionFactory = (itemTypeToComponent, currentPage, onChange) => {
+  const onItemClickFunctionFactory = (value) => {
     return () => {
       if (onChange && currentPage !== value) {
         onChange(value);
       }
-    };
+    }
   };
 
-  return (
-    <div>
-      {
-        paginationModel.map((item, index) => (
-          <button
-            key={item.key}
-            disabled={isDisabled(item)}
-            style={getButtonStyle(item)}
-            onClick={onPageClick(item)}
-          >
-            {getItemLabel(item)}
-          </button>
-        ))
-      }
-    </div>
-  );
+  return (item) => {
+    const ItemComponent = itemTypeToComponent[item.type];
+    const onItemClick = onItemClickFunctionFactory(item.value);
+    return <ItemComponent onClick={onItemClick} {...item}/>
+  }
 };
 
-UltimatePagination.propTypes = {
-  currentPage: React.PropTypes.number.isRequired,
-  totalPages: React.PropTypes.number.isRequired,
-  onChange: React.PropTypes.func
+export const createUltimatePagination = (itemTypeToComponent) => {
+  const UltimatePaginationComponent = ({currentPage, totalPages, onChange}) => {
+    const paginationModel = getPaginationModel({currentPage, totalPages});
+    const renderItemComponent = renderItemComponentFunctionFactory(itemTypeToComponent, currentPage, onChange);
+    return <div>{paginationModel.map(renderItemComponent)}</div>;
+  };
+
+  UltimatePaginationComponent.propTypes = {
+    currentPage: React.PropTypes.number.isRequired,
+    totalPages: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func
+  };
+
+  return UltimatePaginationComponent;
 };
 
-export {UltimatePagination};
+export {ITEM_TYPES};
