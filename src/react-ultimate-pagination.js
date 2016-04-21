745ceb3d9ad7d69c7d59338f74e9092ce1586a4b
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-21 19:25:23 +0300

    Use ultimate-pagination 0.4.0

--- src/react-ultimate-pagination.js
+++ src/react-ultimate-pagination.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import {getPaginationModel, ITEM_TYPES} from 'ultimate-pagination';
 
-const activeButtonStyle = {fontWeight: 'bold'};
+const activePageButtonStyle = {fontWeight: 'bold'};
+
+const LABEL_BY_ITEM_TYPE = {
+  [ITEM_TYPES.ELLIPSIS]: '...',
+  [ITEM_TYPES.FIRST_PAGE_LINK] : '<<',
+  [ITEM_TYPES.PREVIOS_PAGE_LINK] : '<',
+  [ITEM_TYPES.NEXT_PAGE_LINK] : '>',
+  [ITEM_TYPES.LAST_PAGE_LINK] : '>>'
+};
+
+const isActivePage = (item) => item.isActive && item.type === ITEM_TYPES.PAGE;
+const isDisabled = (item) => item.type === ITEM_TYPES.ELLIPSIS || (item.type !== ITEM_TYPES.PAGE && item.isActive);
+const getButtonStyle = (item) => isActivePage(item) ? activePageButtonStyle : null;
+const getItemLabel = (item) => item.type === ITEM_TYPES.PAGE ? item.value : LABEL_BY_ITEM_TYPE[item.type];
 
 const UltimatePagination = ({currentPage, totalPages, onChange}) => {
-  const paginationModel = getPaginationModel(currentPage, totalPages);
-  const onPageClick = (newPage) => {
+  const paginationModel = getPaginationModel({currentPage, totalPages});
+  const onPageClick = ({value}) => {
     return () => {
-      if (onChange && currentPage !== newPage) {
-        onChange(newPage);
+      if (onChange && currentPage !== value) {
+        onChange(value);
       }
     };
   };
@@ -18,12 +31,12 @@ const UltimatePagination = ({currentPage, totalPages, onChange}) => {
       {
         paginationModel.map((item, index) => (
           <button
-            key={item.value}
-            disabled={item.type === ITEM_TYPES.ELLIPSIS}
-            style={item.isActive ? activeButtonStyle : null}
-            onClick={onPageClick(item.value)}
+            key={item.key}
+            disabled={isDisabled(item)}
+            style={getButtonStyle(item)}
+            onClick={onPageClick(item)}
           >
-            {item.type === ITEM_TYPES.PAGE ? item.value : '...'}
+            {getItemLabel(item)}
           </button>
         ))
       }
